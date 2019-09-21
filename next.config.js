7760c2279ed9c117ebe9e4c8d64adfcb0e37d3c3
Fredrik Stave <fredrik.stave@e24.no>
2019-09-21 15:37:23 +0200

    Remove Sentry from Next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
-const withSass = require('@zeit/next-sass')
-const Sentry = require('@sentry/browser');
-
-Sentry.init({
- dsn: 'https://e29f7dba2eee4fa6ad42954cb80e3df7@sentry.io/1465918',
- environment: process.env.NODE_ENV,
- debug: process.env.NODE_ENV === 'development',
-});
+const withSass = require('@zeit/next-sass');
 
 module.exports = withSass({
   cssModules: true,
