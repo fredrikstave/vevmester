f6a5ae70f0761df853be94ee2f4e7f3ddd7507e3
Fredrik Stave <fredrik.stave@e24.no>
2020-01-16 01:26:38 +0100

    Fix fonts

--- pages/_document.js
+++ pages/_document.js
@@ -6,12 +6,9 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="icon" type="image/png" href="/static/favicon.png" sizes="16x16" />
-
-          <link rel="preload" as="style" onload="this.onload=null;this.rel='stylesheet'" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,600,600i,700,700i,900,900i&display=swap" />
           <link rel="preload" as="style" onload="this.onload=null;this.rel='stylesheet'" href="https://fonts.googleapis.com/css?family=Archivo+Black&display=swap" />
           <link rel="preload" as="style" onload="this.onload=null;this.rel='stylesheet'" href="https://fonts.googleapis.com/css?family=Lato&display=swap" />
           <noscript>
-            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,600,600i,700,700i,900,900i&display=swap" />
             <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Archivo+Black&display=swap" />
             <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato&display=swap" />
           </noscript>
