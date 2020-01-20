a598e95fd3c5919d6535e722d6e2cc440bc6642f
Fredrik Stave <fredrik.stave@e24.no>
2020-01-20 23:38:16 +0100

    Added Ghost API connection

--- pages/artikler.js
+++ pages/artikler.js
@@ -1,7 +1,17 @@
 import Head from 'next/head';
+import Link from 'next/link';
+import GhostContentAPI from "@tryghost/content-api";
 import LeftMenu from '../src/components/LeftMenu';
+import styles from './styles/index.scss';
 
-function Main() {
+// Create API instance with site credentials
+const api = new GhostContentAPI({
+  url: 'http://178.128.163.168',
+  key: 'b7bae9e9908a830d6669e65c14',
+  version: "v3"
+});
+
+function Main({ posts = [] }) {
   return [
     <Head key="main-head">
       <title>Vevmester</title>
@@ -9,8 +19,31 @@ function Main() {
     </Head>,
     <main key="main-content">
       <LeftMenu />
+      <div className={styles.listContainer}>
+        <ul>
+          {posts.map(post => (
+            <li key={post.id}>
+              <Link href={`/artikler/[slug]`} as={`/artikler/${post.slug}`}>
+                <a>{post.title}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </main>,
   ];
+}
+
+Main.getInitialProps = async ({ query, res }) => {
+  const posts = await api.posts
+    .browse({
+      limit: "all"
+    })
+    .catch(err => {
+      console.error(err);
+    });
+
+  return { posts };
 }
 
 export default Main;
--- pages/artikler/[...slug].js
+++ pages/artikler/[...slug].js
@@ -1,37 +1,52 @@
 import { useRouter } from 'next/router';
 import Head from 'next/head';
+import GhostContentAPI from "@tryghost/content-api";
 import LeftMenu from '../../src/components/LeftMenu';
 import styles from './styles.scss';
 
-function Main() {
+// Create API instance with site credentials
+const api = new GhostContentAPI({
+  url: 'http://178.128.163.168',
+  key: 'b7bae9e9908a830d6669e65c14',
+  version: "v3"
+});
+
+function Main(props) {
   const router = useRouter()
   const { slug } = router.query;
 
   return [
     <Head key="main-head">
-      <title>Vevmester</title>
-      <meta name="description" content="Showcase website" />
+      <title>{props.title} | Vevmester</title>
+      <meta name="description" content={props.excerpt} />
     </Head>,
     <main key="main-content" className={styles.wrapper}>
       <LeftMenu />
       <div className={styles.articleContainer}>
         <article className={styles.article}>
-          <h1 className={styles.title}>This is a title</h1>
-          <strong className={styles.lead}>This is a little description of the article that should be short and to the point</strong>
-          <em className={styles.byline}>Av Fredrik Stave</em>
-          <p className={styles.paragraph}>Over 92% of computers are infected with Adware and spyware. Such software is rarely accompanied by uninstall utility and even when it is it almost always leaves broken Windows Registry keys behind it. Even if you have an anti-spyware tool your Windows Registry might be broken – developers of those tools are focused on removing Adware and spyware functionality, not every trace of software itself.</p>
-          <p className={styles.paragraph}>Another category of software that is known to leave bits and pieces behind on uninstallation is games. There are a lot of special installation systems that creates strange files, unique entries in your registry file as well as changes system dll’s to other versions. Once you uninstall the game a lot of the special installation changes it made will be left behind, causing you problems.</p>
-          <p className={styles.paragraph}>You should run an advanced registry cleaner for Windows that allows you to safely scan, clean, and repair registry problems with a few simple mouse clicks on a regular basis.</p>
-          <p className={styles.paragraph}>Problems with the Windows Registry are a common cause of Windows crashes (blue screens) and error messages (memory as well as disk errors). By using a PC tune-up utility, your system will be more stable, your software will run quicker, and your operating system will boot faster.</p>
-          <p className={styles.paragraph}>Registry problems can occur for many reasons, including references left behind after uninstallation; incorrect removal of software; missing or corrupt hardware drivers; or orphaned start-up programs. With a few easy steps, a good registry cleaner will scan your entire registry for any invalid entries and provide a list of the errors found.</p>
-          <h2 className={styles.heading}>Type something</h2>
-          <p className={styles.paragraph}>Over 92% of computers are infected with Adware and spyware. Such software is rarely accompanied by uninstall utility and even when it is it almost always leaves broken Windows Registry keys behind it. Even if you have an anti-spyware tool your Windows Registry might be broken – developers of those tools are focused on removing Adware and spyware functionality, not every trace of software itself.</p>
-          <p className={styles.paragraph}>Another category of software that is known to leave bits and pieces behind on uninstallation is games. There are a lot of special installation systems that creates strange files, unique entries in your registry file as well as changes system dll’s to other versions. Once you uninstall the game a lot of the special installation changes it made will be left behind, causing you problems.</p>
-          <p className={styles.paragraph}>You should run an advanced registry cleaner for Windows that allows you to safely scan, clean, and repair registry problems with a few simple mouse clicks on a regular basis.</p>
+          <h1 className={styles.title}>{props.title}</h1>
+          <strong className={styles.lead}>{props.excerpt}</strong>
+          <em className={styles.byline}>Av {props.primary_author.name}</em>
+          <div dangerouslySetInnerHTML={{ __html: props.html}} />
         </article>
       </div>
     </main>,
   ];
+}
+
+Main.getInitialProps = async ({ query, res }) => {
+  const response = await api.posts
+    .read({ slug: query.slug, include: ['authors'] })
+    .catch(err => {
+      res.statusCode = 404;
+      return {
+        err: {
+          statusCode: 404,
+        },
+      };
+    });
+
+  return response;
 }
 
 export default Main;
